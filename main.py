f1bbc6b21f1024be2cccbc84965a471a72764f34
rodX111 <73860161+rodX111@users.noreply.github.com>
2026-06-16 23:45:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Depends, Header
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from pydantic import BaseModel 
 from sqlalchemy import create_engine, text
 import pandas as pd
 import hashlib
